80b2bc2f4bd95094368483251d967b117b3f56fb
Alejandro <69826984+MagiiiKarp@users.noreply.github.com>
2020-11-24 15:48:55 +0000

    Add files via upload

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move down 4 metres per secound
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+
+        //if bottom of screen
+        //respawn at the top with a new random x position
+
+        if (transform.position.y < -5f)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit: " + other.transform.name);
+
+        //if other is player
+        //damage the player
+        //Destroy Us
+
+        //if other is laser
+        //laser
+        //destroy us
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Player : MonoBehaviour
+{
+    //public or private reference
+    //date type (int, float, bool, string)
+    //every variable has a name
+    //optional value assigned
+    [SerializeField]
+    private float _speed = 3.5f;
+    public float horizontalInput;
+    [SerializeField]
+    public GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canfire = -1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //take the current position = new position (0, 0, 0,)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canfire)
+        {
+            FireLaser();
+        }
+
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //new Vector3(-5, 0, 0) * 5 * real time
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        //if the player position on the y is greater than 0
+        //y position = 0
+        //else if position on the y is less than -3.8f
+        //y pos = -3.8f
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        //if player on the x > 11
+        //x pos = -11
+        //else if player on the x is less than -11
+        //x pos = 11
+
+        if (transform.position.x > 11.2f)
+        {
+            transform.position = new Vector3(-11.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.2f)
+        {
+            transform.position = new Vector3(11.2f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        //if i hit the space key
+        //spawn gameObject
+        {
+            _canfire = Time.time + _fireRate;
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.50f, 0), Quaternion.identity);
+        }
+    }
+}
